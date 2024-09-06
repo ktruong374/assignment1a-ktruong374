4a5ff4fc283846700dc6c8c36dce5867fd4d2e4a
ktruong374 <kqt5421@psu.edu>
2024-09-06 14:50:33 -0400

    Assignment 1 Done

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "ktruong374";
+    char STUDENT_ID[] = "928035990";
 
     return 0 ;
 }
